2aaa49c7e28afcf1463ac99d92505ce2f668832f
svanrj <kenvinfei@outlook.com>
2021-08-17 16:17:32 +0800

    jwt token

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,25 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const { autoLoadRouter } = require('./util/authLoader')
-require('./util/mysqlInit')
-app.use(express.json())
-autoLoadRouter(app, path.join(__dirname, 'controller'))
+const express = require("express");
+const path = require("path");
+const app = express();
+var { Verification } = require("./util/jwt");
+const { autoLoadRouter } = require("./util/authLoader");
+
+require("./util/mysqlInit");
+app.use(express.json());
+
+app.use((req: any, res: any, next: any) => {
+  let token = req.query.token;
+  if (req.path == "login") {
+    next();
+  }
+  if (Verification(token)) {
+    res.send({ message: "认证失败", code: 400 });
+  } else {
+    next();
+  }
+});
+autoLoadRouter(app, path.join(__dirname, "controller"));
 
 app.listen(3000, () => {
-  console.log("server start! OK!")
-})
+  console.log("server start! OK!");
+});
---
+++ server/util/jwt.ts
@@ -0,0 +1,39 @@
+const jwt = require("node.jwt");
+
+const secret = jwt.secret("svanrj");
+
+var createJwtToken: any = (uid: string) => {
+  const payload = {
+    sub: "svanrj",
+    iss: "https://github.com/sanqi377/societies",
+    exp: Date.now() / 1000 + 60 * 60 * 24 * 2,
+    nbf: Date.now() / 1000 - 60 * 5,
+    iat: Date.now() / 1000,
+    data: {
+      uid: uid,
+      authority: new Date().getTime(),
+    },
+  };
+
+  var token = jwt.encode(payload, secret);
+
+  return token;
+};
+
+
+var Verification: any = (token: string) => {
+    if (token) {
+        try {
+            var decoded = jwt.decode(token,secret);
+        } catch (err) {
+            return false
+        }
+        return true
+    }
+    return false
+};
+
+module.exports = {
+  createJwtToken,
+  Verification
+};
