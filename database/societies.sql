f0f6d57ce430237beafca33d878ce1a96a7a8874
svanrj <kenvinfei@outlook.com>
2021-08-17 20:02:46 +0800

    update sql file

--- database/societies.sql
+++ database/societies.sql
@@ -1,36 +1,77 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50726
-Source Host           : localhost:3306
-Source Database       : societies
+ Source Server         : mysql5
+ Source Server Type    : MySQL
+ Source Server Version : 50726
+ Source Host           : localhost:3306
+ Source Schema         : societies
 
-Target Server Type    : MYSQL
-Target Server Version : 50726
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50726
+ File Encoding         : 65001
 
-Date: 2021-08-17 13:37:21
+ Date: 17/08/2021 19:59:45
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for s_college
+-- ----------------------------
+DROP TABLE IF EXISTS `s_college`;
+CREATE TABLE `s_college`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '社团ID',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '社团名称',
+  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '社团头像',
+  `notice` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '公告',
+  `number` int(11) NOT NULL COMMENT '社团人数',
+  `department` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '所属院系',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of s_college
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for s_task
+-- ----------------------------
+DROP TABLE IF EXISTS `s_task`;
+CREATE TABLE `s_task`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '任务id',
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '任务标题',
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '任务内容',
+  `publisher_college_id` int(11) NOT NULL COMMENT '发布社团',
+  `recipient_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '接受者id,可多人',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `publisher_college_id`(`publisher_college_id`) USING BTREE,
+  CONSTRAINT `publisher_college_id` FOREIGN KEY (`publisher_college_id`) REFERENCES `s_college` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of s_task
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for s_users
 -- ----------------------------
 DROP TABLE IF EXISTS `s_users`;
-CREATE TABLE `s_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `open_id` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `avatar` varchar(255) DEFAULT NULL,
-  `class` varchar(255) DEFAULT NULL,
-  `departments` varchar(255) DEFAULT NULL,
-  `student_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+CREATE TABLE `s_users`  (
+  `id` int(11) NOT NULL,
+  `open_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `class` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `departments` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `student_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `college_id` int(11) NOT NULL DEFAULT -1 COMMENT '所属社团，为-1时为新人用户',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of s_users
 -- ----------------------------
-INSERT INTO `s_users` VALUES ('15', 'oCyJb4xF_IpSqyyqn5kEiGG7UxeM', '叁柒', null, 'XX2021', '信息工程系', '123456');
+
+SET FOREIGN_KEY_CHECKS = 1;
--- server/middleware/index/checkLogin.ts
+++ server/middleware/index/checkLogin.ts
@@ -2,7 +2,7 @@ var { Verification } = require("../../util/jwt");
 var init = (app: any) => {
   app.use((req: any, res: any, next: any) => {
     let token = req.query.token;
-    if (req.path == "login") {
+    if (req.path == "login"||req.path=="req") {
       next();
     }
     if (Verification(token)) {
