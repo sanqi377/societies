058a6c940ebbed532942577ab137a7e374e6b960
svanrj <kenvinfei@outlook.com>
2021-08-20 14:09:11 +0800

    角色权限初步完成

--- server/model/admin/System.ts
+++ server/model/admin/System.ts
@@ -1,41 +1,66 @@
-var { db } = require('../../util/mysqlInit')
+var { db } = require("../../util/mysqlInit");
 module.exports = {
   /**
    * 后台获取菜单
+   * @param menus 菜单id
    */
-  getMenu() {
+  getMenu(menus: any) {
+    let id=menus.sign.split(":")
+    console.log(id);
     return new Promise((resolve) => {
-      db('s_menu').where({ pid: 0, status: 1 }).order({ 'sort': 'asc' }).select().then((res: any) => {
-        for (let i = 0; i < res.length; i++) {
-          res[i].children = []
-          db('s_menu').where({ pid: res[i].id, status: 1 }).order({ 'sort': 'asc' }).select().then((resp: any) => {
-            res[i].children = resp
-            if (i === res.length - 1) resolve(res)
-          })
-        }
-      })
-    })
+        db("s_menu")
+          .where({ pid: 0, status:1 })
+          .order({ sort: "asc" })
+          .select()
+          .then((res: any) => {
+            for (let i = 0; i < res.length; i++) {
+              res[i].children = [];
+              db("s_menu")
+                .where({ pid: res[i].id, status: 1 })
+                .order({ sort: "asc" })
+                .select()
+                .then((resp: any) => {
+                  res[i].children = resp;
+                  let menu:any=[]
+                  res.forEach((el:any) => {
+                    for(let i=0;i<id.length;i++){
+                      if(el.id==id[i]){
+                        menu.push(el)
+                      }
+                    }
+                  });
+                  if (i === res.length - 1) resolve(menu);
+                });
+            }
+          });
+    });
   },
   /**
    * 后台删除菜单
    * @param id 菜单id
    */
   deleteMenu(id: number) {
-    return db('s_menu').where({ id }).delete()
+    return db("s_menu").where({ id }).delete();
   },
   /**
    * 后台修改菜单
    * @param data 修改内容
    */
   reviseMenu(data: any) {
-    delete data.children
-    return db('s_menu').where({ id: data.id }).update(data)
+    delete data.children;
+    return db("s_menu").where({ id: data.id }).update(data);
   },
   /**
    * 新增修改菜单
    * @param data 修改内容
    */
   addMenu(data: any) {
-    return db('s_menu').insert(data)
-  }
-}
+    return db("s_menu").insert(data);
+  },
+  /**
+   * 获取角色是否拥有当前菜单权限
+   */
+  getUserMenu(id: number) {
+    return db("s_powers").where({ id }).find();
+  },
+};
